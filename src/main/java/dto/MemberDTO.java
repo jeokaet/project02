035988fb50a780d3ac22801ecbd2065ec04e1317
jeokaet <whgnldud03252naver.com>
2025-02-13 15:42:01 +0900

    jeokaet MemberController

--- src/main/java/dto/MemberDTO.java
+++ src/main/java/dto/MemberDTO.java
@@ -1,5 +1,12 @@
 package dto;
 
 public class MemberDTO {
-
+private String id;
+private String password;
+private String name;
+private String email;
+private String phone;
+private int postcode;
+private String address1;
+private String address2;
 }
